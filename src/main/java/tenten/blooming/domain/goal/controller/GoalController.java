4569ba7be506230ce45ca6a625c276aea7faaccb
Lee Haewon <hawon2205@ewhain.net>
2024-01-11 02:24:39 +0900

    feat: 목표 등록 구현 #3

--- src/main/java/tenten/blooming/domain/goal/controller/GoalController.java
+++ src/main/java/tenten/blooming/domain/goal/controller/GoalController.java
@@ -1,12 +1,18 @@
 package tenten.blooming.domain.goal.controller;
+import jakarta.persistence.Basic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 import tenten.blooming.domain.goal.dto.ChatRequest;
 import tenten.blooming.domain.goal.dto.ChatResponse;
-
+import tenten.blooming.domain.goal.dto.GoalDto;
+import tenten.blooming.domain.goal.entity.Goal;
+import tenten.blooming.domain.goal.service.GoalService;
+import tenten.blooming.global.common.BasicResponse;
 
 @RestController
 public class GoalController {
@@ -15,11 +21,32 @@ public class GoalController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private GoalService goalService;
+
     @Value("${openai.model}")
     private String model;
 
     @Value("${openai.api.url}")
     private String apiUrl;
+
+    @PostMapping("/goal")
+    public ResponseEntity<BasicResponse> createGoal(@RequestBody GoalDto dto) {
+        Goal created = goalService.create(dto);
+        BasicResponse basicResponse = new BasicResponse();
+
+        if (created != null) {
+            basicResponse = BasicResponse.builder()
+                    .code(HttpStatus.OK.value())
+                    .message("목표 등록에 성공했습니다.")
+                    .result(created)
+                    .build();
+
+        }
+
+        return new ResponseEntity<>(basicResponse, HttpStatus.OK);
+
+    }
 
     @GetMapping("/goal")
     public String getSubGoal(@RequestParam String goalName) {
